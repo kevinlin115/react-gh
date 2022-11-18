6dc0acf6b750bf7043b06da03d13aff22a87e184
Kevin <nash4thewin@gmail.com>
2022-11-18 22:46:57 +0800

    Footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import './footer.css';
+
+interface FooterProps {}
+
+const Footer: FC<FooterProps> = () => (
+  <div className="footer">
+    Footer Component
+  </div>
+);
+
+export default Footer;
